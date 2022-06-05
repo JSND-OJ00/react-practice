f204f92834c33601f1554a62faa685f4ad8e19d8
JSND-OJ00 <jsnd.oj@gmail.com>
2022-06-05 16:20:52 +0900

    0605

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,26 @@
-import React from 'react';
-
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React from "react";
+import { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const handleTitle = () => {
+    setTitle("new title");
+  };
+
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={handleTitle}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
